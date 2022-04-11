8f4e07decf5dfa9cd4d15e59211f837ac30d393a
mfragger <58777037+mfragger@users.noreply.github.com>
2022-04-11 20:00:41 +0800

    tbc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,8 @@ export class AppComponent {
   public newMemberName: string = "";
   public members: string[] = [];
   public errorMessage: string = "";
+  public numberOfTeams: number | "" = "";
+  private teams: string[][] = []
 
   public addMember(): void {
     if (!this.newMemberName) {
@@ -23,5 +25,32 @@ export class AppComponent {
 
   public onInput(member: string): void {
     this.newMemberName = member;
+  }
+
+  public onNumberOfTeamsInput(numberOfTeams: string) {
+    var num: number = Number(numberOfTeams);
+    this.numberOfTeams = num;
+  }
+  public generateTeams(): void {
+
+    //wth, why is there so many guard clauses?
+    if (!this.numberOfTeams || this.numberOfTeams <= 0) {
+      return;
+    }
+    const allMembers = [...this.members];
+
+    const max = this.numberOfTeams;
+
+    for (let i = 0; i < max; i++) {
+      const randNumber = Math.floor(Math.random() * max);
+      const member = allMembers.splice(randNumber, 1)[0];
+
+      if (this.teams[i]) {
+        this.teams[i].push(member);
+      } else {
+        this.teams[i] = [member];
+      }
+    }
+    console.log(this.teams);
   }
 }
